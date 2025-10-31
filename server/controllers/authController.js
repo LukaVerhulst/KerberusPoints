d1c0d4846e5bbe76a4eda48f61f629e5859129d6
LukaVerhulst <luka.verhulst2007@gmail.com>
2025-10-31 21:08:28 +0100

    fix cookies in authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -32,8 +32,8 @@ export const login = (req, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
